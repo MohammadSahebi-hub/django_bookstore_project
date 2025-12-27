4f92349133ff408e9cdfa127947b4420e02de253
Mohammad Sahebi <msahebi.me@gmail.com>
2025-12-27 13:52:57 +0330

    add comment form to book detail view

--- books/views.py
+++ books/views.py
@@ -4,12 +4,15 @@ from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
 
 from .models import Book
+from .forms import CommentForm
+
 
 class BookListView(generic.ListView):
     model = Book
     paginate_by = 4
     template_name = 'books/book_list.html'
     context_object_name = 'books'
+
 
 # class BookDetailView(generic.DetailView):
 #     model = Book
@@ -20,17 +23,36 @@ def book_detail_view(request, pk):
     book = get_object_or_404(Book, pk=pk)
     # get book comments
     book_comments = book.comments.all()
-    return render(request, 'books/book_detail.html', {'book': book, 'comments': book_comments})
+
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.book = book
+            new_comment.user = request.user
+            new_comment.save()
+            comment_form = CommentForm()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'books/book_detail.html',{
+        'book': book,
+        'comments': book_comments,
+        'comment_form': comment_form,
+    })
+
 
 class BookCreateView(generic.CreateView):
     model = Book
-    fields = ['title', 'author', 'description', 'price', 'cover']
+    fields = ['title', 'author', 'description', 'price', 'cover',]
     template_name = 'books/book_create.html'
+
 
 class BookUpdateView(generic.UpdateView):
     model = Book
-    fields = ['title', 'author', 'description', 'price', 'cover']
+    fields = ['title', 'author', 'description', 'price', 'cover',]
     template_name = 'books/book_update.html'
+
 
 class BookDeleteView(generic.DeleteView):
     model = Book
